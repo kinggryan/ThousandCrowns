410aef3738bb5635b93c9b98e258d826f5b7b6b9
Taylor Chen <haoyao1993@gmail.com>
2014-10-31 18:53:14 -0700

    Done Game End Functionality

--- Assets/Conflict/System/ConflictTurnController.js
+++ Assets/Conflict/System/ConflictTurnController.js
@@ -22,7 +22,7 @@ class ConflictTurnController extends Photon.MonoBehaviour {
 		// Display "Done" Button
 		var buttonPosition = Rect(750,15,50,50);
 		if(!turnDone) {
-			if(GUI.Button(buttonPosition,"Done")) {
+			if(GUI.Button(buttonPosition,"Done")) { //RETURNS TRUE when user clicks on the "Done" button
 				turnDone = true;
 				photonView.RPC("PlayerTurnDone",PhotonTargets.All);
 			}
@@ -58,7 +58,43 @@ class ConflictTurnController extends Photon.MonoBehaviour {
 		numberOfDonePlayers = 0;
 		
 		// TODO add end game method call here
+		if(ConflictSite.uncapturedSitesRemaining == 0){  
+		EndGame();
+		}
 	}
+	
+	function EndGame(){
+		var scores = [0, 0, 0, 0, 0]; // initializes all players' scores to 0
+		var i = 0;
+		var j = 0;
+		var max = 0;
+		var maxIndex = 0;
+		var playerIndex = 0; // used to keep track of which player we are dealing with
+		var playerManager = GameObject.FindObjectOfType(ConflictPlayerManager) as ConflictPlayerManager; 
+
+		//traverses all sites in the game and calculate the scores of players
+		for(var site in GameObject.FindObjectsOfType(ConflictSite) as ConflictSite[]){ 
+			playerIndex = playerManager.playerList.IndexOf(site.controllingPlayer);
+			scores[playerIndex]++;
+		}
+		
+		//This for loop displays the players in order of their scores
+		for(i = 0; i < 5; i++){
+			max = scores[0];
+			maxIndex = 0;
+			for(j = 1; j < 5; j++){
+				if(scores[j] > max){
+					max = scores[j];
+					maxIndex = j;
+				}
+			}
+			ConflictLog.LogMessage("No. " + (i + 1) + " " + playerManager.playerColorStringList[maxIndex] + " " + max);
+			scores[maxIndex] = -100;		
+		}
+		
+		gameObject.AddComponent(ReturnToGameLobby); //shows the "return to game lobby" button
+	}
+
 	
 	// MARK: RPCs
 	@RPC
---
+++ Assets/Conflict/System/ReturnToGameLobby.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+class ReturnToGameLobby extends MonoBehaviour{
+
+function OnGUI()
+    {    
+    	if(GUI.Button(Rect(15,15,100,100),"Game Lobby"))
+    		PhotonNetwork.LoadLevel(0);	
+
+    }
+
+}
